eea30b509685272904a91fced3bdf88234f3f255
evlbe <150168123+evlbe@users.noreply.github.com>
2024-11-21 13:31:37 -0700

    Objectives Basic
    Bug where player cant move through objectives is back

---
+++ 22a-Psyche-M-Type-Simu/Assets/Objectives/ObjectiveEnd.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ObjectiveComplete : MonoBehaviour
+{
+    public bool Complete;
+    public string TextComplete;
+    public TextMeshProUGUI Text;
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Complete = true;
+            Text.text = TextComplete.ToString();
+            StartCoroutine("WaitForSec");
+        }
+    }
+    void Start()
+    {
+        Text = Text.GetComponent<TextMeshProUGUI>();
+    }
+    public IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(3);
+        DestroyImmediate(Text);
+    }
+
+
+
+
+}
