7c6b0002861af208848093bc24c1c07dca26cf84
Will Nelson <nelsonwill254@gmail.com>
2025-01-28 13:14:41 -0700

    implemented options, mission 1 start screen, and contribution/disclaimer text
    Implemented the titled systems.
    
    other implementations/fixes:
    -adjusted starting camera
    -disabled camera movement when game is paused
    -start screen pauses all player animations, camera controls, and movement

--- 22a-Psyche-M-Type-Simu/Assets/C# Script/CameraControls.cs
+++ 22a-Psyche-M-Type-Simu/Assets/C# Script/CameraControls.cs
@@ -24,6 +24,12 @@ public class CameraControls : MonoBehaviour
     private float currentYRotation; // y-axis rotation angle (pitch)
     private float currentXRotation; // x-axis rotation angle (yaw)
 
+    //camera controller options function
+    public void OptionsCameraSpeed (float newSpeed)
+    {
+        rotationSpeed = newSpeed;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
