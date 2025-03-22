52c4677f933b9ca7f326f183737a863e30364306
Will Nelson <nelsonwill254@gmail.com>
2025-03-22 15:09:16 -0700

    Camera UI added
    UI to the camera mode has been added in mission 2 , this includes the following changes while in camera mode:
    -disables map ui
    -enables camera frame ui
    -text display to updater player when the image objective is in frame

--- 22a-Psyche-M-Type-Simu/Assets/C# Script/Miss2Interactor.cs
+++ 22a-Psyche-M-Type-Simu/Assets/C# Script/Miss2Interactor.cs
@@ -19,7 +19,7 @@ public class Miss2Interactor : MonoBehaviour
 
     [SerializeField] public Camera mainCamera;
     [SerializeField] public Camera camMode;
-    private bool isCamModeActive = false;
+    [SerializeField] private bool isCamModeActive = false;
 
     [SerializeField] private GameObject target1;
     [SerializeField] private GameObject target2;
@@ -30,6 +30,12 @@ public class Miss2Interactor : MonoBehaviour
     [SerializeField] protected GameObject Status3;
     [SerializeField] protected GameObject LevelCompleteScreen;
     [SerializeField] private GameObject roverCAMmodel;
+    [SerializeField] private GameObject mapmaskUI;
+    [SerializeField] private GameObject mapoutlineUI;
+    [SerializeField] private GameObject ObjectInView;
+    [SerializeField] private GameObject ObjectOutView;
+    [SerializeField] private GameObject CamFrame;
+    bool inCamView = false;
 
 
     //ObjectCameraDetection script2 = target2.GetComponent<ObjectCameraDetection>();
@@ -77,7 +83,7 @@ public class Miss2Interactor : MonoBehaviour
 
                 //disables movement till animation is complete
                 movementScript.enabled = false;
-                roverCAMmodel.SetActive(false);
+                
 
                 //check if all objectives have been completed
                 
@@ -108,9 +114,43 @@ public class Miss2Interactor : MonoBehaviour
             //disables display UI for what key to press and ensures it remains off till needed
             ControlPrompt.SetActive(false);
         }
+
+
         if (interactionCount >= 3)
         {
            LevelCompleteRoutine();
+        }
+
+
+        //ui changes
+        if (isCamModeActive == true)
+        {
+            inCamView = visCheck();
+
+            CamFrame.SetActive(true);
+            roverCAMmodel.SetActive(false) ;
+            mapmaskUI.SetActive(false);
+            mapoutlineUI.SetActive(false);
+
+            if (inCamView)
+            {
+                ObjectInView.SetActive(true);
+                ObjectOutView.SetActive(false);
+            }
+            else
+            {
+                ObjectOutView.SetActive(true);
+                ObjectInView.SetActive(false);
+            }
+        }
+        else
+        {
+            CamFrame.SetActive(false);
+            roverCAMmodel.SetActive(true);
+            mapmaskUI.SetActive(true);
+            mapoutlineUI.SetActive(true);
+            ObjectOutView.SetActive(false);
+            ObjectInView.SetActive(false);
         }
     }
 
