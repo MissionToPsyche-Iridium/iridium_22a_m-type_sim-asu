0578c39e4f76690ea74845d033b270e080843d7d
Will Nelson <nelsonwill254@gmail.com>
2025-03-26 14:57:01 -0700

    fix bugged with level transition
    fixed bug when time.scale stayed on 0 when transitioning  levels

--- 22a-Psyche-M-Type-Simu/Assets/C# Script/NextLevel.cs
+++ 22a-Psyche-M-Type-Simu/Assets/C# Script/NextLevel.cs
@@ -7,6 +7,7 @@ public class NextLevel : MonoBehaviour
 {
     public void nextscene()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
