42221de95f0406f6fe68c659e701e576166c701c
Will Nelson <nelsonwill254@gmail.com>
2025-04-03 16:07:53 -0700

    Added Openable Map Toggle
    Player can now open the map by pressing [M]. This allows the player to view current position on map relative to the map and its objectives

---
+++ 22a-Psyche-M-Type-Simu/Assets/C# Script/MapFunciton.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
+
+public class MapFunciton : MonoBehaviour
+{
+    [SerializeField] protected GameObject mapUI;
+    [SerializeField] bool isMapEnabled;
+    private float lastButtonPress = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMapEnabled = false;
+        mapUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if m button is pressed and has been greater than 0.3 seconds
+        if (Input.GetKey(KeyCode.M) && (Time.unscaledTime - lastButtonPress > 0.3f))
+        {
+            if (isMapEnabled)
+            { //closes map and unfreezes the scene when m is pressed
+                mapUI.SetActive(false);
+                isMapEnabled = false;
+                Time.timeScale = 1f;
+                lastButtonPress = Time.unscaledTime;
+            }
+            else
+            { //opens map and freezes scene whenm m is pressed
+                mapUI.SetActive(true);
+                isMapEnabled = true;
+                Time.timeScale = 0f;
+                lastButtonPress = Time.unscaledTime;
+            }
+        }
+    }
+}
